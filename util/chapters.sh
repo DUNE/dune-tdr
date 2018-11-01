30fdb612dd27a10b3acaeffb8c964e8048024a78
Brett Viren <brett.viren@gmail.com>
2018-11-01 16:46:08 -0400

    Add an 'if' for SP and DP to allow hiding of 'wrong sign' tex.

--- util/chapters.sh
+++ util/chapters.sh
@@ -14,6 +14,15 @@ out=$1 ; shift
 voltit="$1" ; shift
 chtit="$1" ; shift
 volnum="$1"; shift
+ifmodule=""
+
+# magic numbers matching 1+index of voltexs in wscript
+if [ "$volnum" = 3 ] ; then
+    ifmodule='\\sptrue'
+fi
+if [ "$volnum" = 4 ] ; then
+    ifmodule='\\dptrue'
+fi
 
 volume=$(basename $(dirname $tex))
 chapter=$(basename $tex .tex)
@@ -23,5 +32,7 @@ cat $tmpl | sed -e "s/@volume@/$volume/g" \
                 -e "s/@voltitle@/$voltit/g" \
                 -e "s/@chtitle@/$chtit/g" \
                 -e "s/@volnumber@/$volnum/g" \
+                -e "s/@ifmodule@/$ifmodule/g" \
                 > "${out}"
+
 
